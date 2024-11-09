65597ba6a58cc442dcd36945716a5b634f50ff48
Huy Cao <caogiahuy615@gmail.com>
2024-11-09 17:57:57 -0500

    Initial Commit

---
+++ case-jobboard/case_jobboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  case_jobboardApp.swift
+//  case-jobboard
+//
+//  Created by Cao Gia Huy on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct case_jobboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ case-jobboardUITests/case_jobboardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  case_jobboardUITestsLaunchTests.swift
+//  case-jobboardUITests
+//
+//  Created by Cao Gia Huy on 11/9/24.
+//
+
+import XCTest
+
+final class case_jobboardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
